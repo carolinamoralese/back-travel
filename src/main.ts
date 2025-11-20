eabee5091374ea4b052c0f12dadea0b528676e9d
carolinamoralese <caromorales1396@gmail.com>
2025-11-19 19:26:47 -0500

    fix:ajuste despliegue documentacion

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('API de Travels')
+    .setTitle('API Travels')
     .setDescription('Documentación de la API con autenticación JWT y roles')
-    .setVersion('1.0')
+    .setVersion('2.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      url: '/api-json',
+    },
+    customCssUrl: ['https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui.css'],
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui-standalone-preset.js',
+    ],
+  };
+  SwaggerModule.setup('api/docs', app, document, customOptions);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
